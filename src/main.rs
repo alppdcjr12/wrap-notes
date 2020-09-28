27b2b49ff9c8ab6d5db33b4a1a5f93b825bbbba1
Peter <petercharlandsilence@gmail.com>
2020-09-28 18:39:00 -0400

    can write and read users from a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::fs::File;
-use std::io::prelude::*;
+// use std::fs::File;
+// use std::io::prelude::*;
 
 fn main() {
-    
+  let info = "".split("#");
+  for i in info {
+    println!("{:?}", i);
+  }
 }
